d52ed16f1ea351c3c16114fe6160b9e6f467d18d
Arthur Neuman <arthurbneuman@gmail.com>
2025-08-29 12:57:22 -0400

    Use `.js` extensions for barrel file

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
 export {
   StatefulMap,
   useMap
-} from './hooks/use-map'
+} from './hooks/use-map.js'
 
 export {
   StatefulSet,
   useSet
-} from './hooks/use-set'
+} from './hooks/use-set.js'
 
 export {
   type PromiseResult,
   usePromise
-} from './hooks/use-promise'
+} from './hooks/use-promise.js'
 
 export {
   useObject
-} from './hooks/use-object'
+} from './hooks/use-object.js'
 
 export {
   useDebouncedState
-} from './hooks/use-debounced-state'
+} from './hooks/use-debounced-state.js'
 
 export {
   useUnsaved
-} from './hooks/use-unsaved'
+} from './hooks/use-unsaved.js'
