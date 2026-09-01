abf7a3ab208d66725a12788186212448921e2d6c
Arthur Neuman <arthurbneuman@gmail.com>
2026-09-01 14:36:31 -0400

    Support nullish types in `useArrayMemo`

--- src/hooks/use-array-memo.ts
+++ src/hooks/use-array-memo.ts
@@ -5,12 +5,17 @@ import { useMemo, useRef } from 'react'
  * @param arr The array to memo
  * @returns   The memoed array
  */
-export function useArrayMemo<T extends readonly any[]> (arr: T): T {
+export function useArrayMemo<T extends readonly any[] | null | undefined> (arr: T): T {
   const memoed = useRef(arr)
 
   // `useMemo` so update runs before everything
   useMemo(() => {
-    if (arr.length !== memoed.current.length || arr.some((e, i) => e !== memoed.current[i])) memoed.current = arr
+    if (!arr || !memoed.current) {
+      memoed.current = arr
+      return
+    }
+
+    if (arr.length !== memoed.current.length || arr.some((e, i) => e !== memoed.current![i])) memoed.current = arr
   }, [arr])
 
   return memoed.current
